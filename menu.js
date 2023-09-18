c0cffbcd385412b9a920cd83dd9dc79fc9142268
Drastiko <115904979+Drastiko@users.noreply.github.com>
2023-09-18 13:47:33 -0500

    Update menu.js

--- menu.js
+++ menu.js
@@ -5,13 +5,13 @@ let menuAbierto = false;
 
 function abrirMenu() {
     menuMovil.style.right = "0";
-    btnMenu.style.right = "250px"; // Ajusta el valor para ajustar la posición del botón
+    btnMenu.style.right = "250px"; 
     menuAbierto = true;
 }
 
 function cerrarMenu() {
     menuMovil.style.right = "-250px";
-    btnMenu.style.right = "0"; // Restaura la posición original del botón cuando se cierra el menú
+    btnMenu.style.right = "0"; 
     menuAbierto = false;
 }
 
@@ -34,7 +34,6 @@ menuMovil.addEventListener("click", function (e) {
     e.stopPropagation(); 
 });
 
-// Cierra el menú si se hace clic en cualquier otro lugar de la página
 document.addEventListener("click", function () {
     if (menuAbierto) {
         cerrarMenu();
